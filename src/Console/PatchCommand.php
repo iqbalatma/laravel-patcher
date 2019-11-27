fbf27014679c893a4b5bea1cb3eb938a4efeb564
Rifki Alhuraibi <veelasky@gmail.com>
2019-11-27 22:58:05 +0700

    override method

--- src/Console/PatchCommand.php
+++ src/Console/PatchCommand.php
@@ -68,6 +68,29 @@ class PatchCommand extends MigrateCommand
     }
 
     /**
+     * Get all of the migration paths.
+     *
+     * @return array
+     */
+    protected function getMigrationPaths()
+    {
+        // Here, we will check to see if a path option has been defined. If it has we will
+        // use the path relative to the root of the installation folder so our database
+        // migrations may be run for any customized path from within the application.
+        if ($this->input->hasOption('path') && $this->option('path')) {
+            return collect($this->option('path'))->map(function ($path) {
+                return ! $this->usingRealPath()
+                    ? $this->laravel->basePath().'/'.$path
+                    : $path;
+            })->all();
+        }
+
+        return array_merge(
+            $this->migrator->paths(), [$this->getMigrationPath()]
+        );
+    }
+
+    /**
      * Get migration path (either specified by '--path' option or default location).
      *
      * @return string
