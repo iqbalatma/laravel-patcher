f1eb909eefeeecce03bf4318539ec489e2cad6a5
Muhajirin <muhajirinlpu@gmail.com>
2020-08-24 14:18:34 +0700

    fix wrong path

--- src/Console/StatusCommand.php
+++ src/Console/StatusCommand.php
@@ -9,4 +9,35 @@ class StatusCommand extends MigrationStatusCommand
     protected $name = 'patcher:status';
 
     protected $description = 'Show the status of each patches.';
+
+    public function handle()
+    {
+        return $this->migrator->usingConnection($this->option('database'), function () {
+            if (! $this->migrator->repositoryExists()) {
+                $this->error('Migration table not found.');
+
+                return 1;
+            }
+
+            $ran = $this->migrator->getRepository()->getRan();
+
+            $batches = $this->migrator->getRepository()->getMigrationBatches();
+
+            if (count($patches = $this->getStatusFor($ran, $batches)) > 0) {
+                $this->table(['Ran?', 'Patch', 'Batch'], $patches);
+            } else {
+                $this->error('No migrations found');
+            }
+        });
+    }
+
+    /**
+     * Get migration path (either specified by '--path' option or default location).
+     *
+     * @return string
+     */
+    protected function getMigrationPath()
+    {
+        return $this->laravel->basePath().DIRECTORY_SEPARATOR.'patches';
+    }
 }
